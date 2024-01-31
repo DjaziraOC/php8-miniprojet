978212acec797d5d0bb4961ef61ca6a20aa7d1c4
DjaziraOC <djaziranait14@gmil.com>
2024-01-31 14:58:55 +0100

    désactiver la suppression des données qui ont des clé étrangère

--- articles.php
+++ articles.php
@@ -3,13 +3,28 @@
   include_once __DIR__ . "/header.php";
   include_once __DIR__ . "/main.php";
   $count = 0;
+// désactiver la suppression des données qui ont des clé étrangère  
+// les idarticles sur la ligne commmande
+  $list=[];
+  // Préparer et exécuter une requête SELECT pour récupérer les id des articles  
+  $query="SELECT idarticle FROM article WHERE idarticle IN (SELECT idarticle FROM ligne_commande WHERE  ligne_commande.idarticle = article.idarticle)";
+  $PDOstatment = $pdo->prepare( $query);
+  $PDOstatment->execute();
+  // var_dump($PDOstatment->fetchAll(mode:PDO::FETCH_NUM));
+  foreach($PDOstatment->fetchAll(mode:PDO::FETCH_NUM) as $tabvalue){
+    foreach($tabvalue as $value){
+       $list[]= $value;
+    }   
+  };
+
+//  var_dump($list);
 //<!-- Affichage SELECT-Afficher tous les clients de la base de données -->
-    // Préparer et exécuter une requête SELECT pour récupérer tous les clients 
-    $query=$pdo->prepare("SELECT * FROM article");
+    // Préparer et exécuter une requête SELECT pour récupérer tous les clients
+    $PDOstatment=$pdo->prepare("SELECT * FROM article");
     // Exécuter la requête
-    $query-> execute();
+    $PDOstatment-> execute();
     // Récupérez tous les résultats sous forme de tableau associatif(où les noms de colonnes servent de clés pour les valeurs correspondantes.)
-    $lignes = $query->fetchAll(mode:PDO::FETCH_ASSOC); 
+    $lignes = $PDOstatment->fetchAll(mode:PDO::FETCH_ASSOC); 
     //Afficher les résultats (Tous les clients)avec foreach appliqué sur <td></td>(foreach $lignes as $ligne){$alllignes = [$ligne['idclient'] , $ligne['nom'], $ligne['prenom'], $ligne['telephone']];}
 ?>
 <!-- Begin page content -->
@@ -40,17 +55,17 @@
           <td><?php echo $ligne["prix_unitaire"]?></td>
           <td> 
             <!-- Bouton pour mettre à jour un article -->
-            <a href="updatarticle.php?idarticle=<?php echo $ligne["idarticle"]?>" type="button" class="btn btn-success">
+            <a href="updatarticle.php?idarticle=<?php echo $ligne["idarticle"]?>" class="btn btn-success">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
                 <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
               </svg>
             </a>
               <!-- Bouton pour ouvrir le modal de suppression -->
-            <a href="deletarticle.php" type="button" class="btn btn-danger"  data-bs-toggle="modal" data-bs-target="#deleteModal<?php echo $count?>">
+            <button type="button" class="btn btn-danger"  data-bs-toggle="modal" <?php if(in_array($ligne["idarticle"],$list)){echo "disabled";}?> data-bs-target="#deleteModal<?php echo $count?>">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                 <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
               </svg>
-            </a>
+            </button>
           </td>
         </tr> 
         <!-- Modal de Suppression -->
@@ -81,7 +96,6 @@
 ?>
 
 
-<!-- désactiver la suppression des données qui ont des clé étrangère  -->
 
 
 
